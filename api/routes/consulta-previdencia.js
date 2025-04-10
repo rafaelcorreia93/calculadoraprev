64439ec5d07778314936203257712af0dfd69bf4
Rafael Correia <rafael93correia@gmail.com>
2025-04-10 01:07:41 -0300

    mudanca endpoint prev

--- api/routes/consulta-previdencia.js
+++ api/routes/consulta-previdencia.js
@@ -13,41 +13,6 @@ router.post('/dados-previdencia', async (req, res) => {
     const token = await hash.gerarToken();
     
     try {
-        console.log('req: '+req.body.cpf);
-        
-        const responseProdutos = await fetch(apiUrlProdutos+ req.body.cpf, {
-            method: 'GET', // Opcional, GET é o padrão
-            headers: {
-                'Accept': 'application/json',
-                'Ocp-Apim-Subscription-Key': process.env.subsciption
-            }
-        });
-        if (!responseProdutos.ok) {
-            // Se não foi OK, tenta ler o corpo do erro como texto
-            const errorBody = await responseProdutos.text();
-            console.error(`Erro ${responseProdutos.status} da API: ${errorBody}`);
-            // Lança um erro para ser pego pelo catch externo
-            throw new Error(`Erro da API externa: ${responseProdutos.status} - ${errorBody.substring(0, 100)}`);
-        }
-        const responseProdutosObj = await responseProdutos.json();
-        if(!responseProdutosObj.data.length) {
-            console.log('sem produto');
-            res.json({'res': 'sem produto'});
-
-        } else {
-            console.log(responseProdutosObj);
-            
-            const produtoPrev = responseProdutosObj.data.find(item => {
-                return item && item.produto === 'Previdencia';
-              });
-            
-            const dadosParaConsultaDetalhe = {
-                cpf: req.body.cpf,
-                empresa: produtoPrev.codigoEmpresa,
-                matricula: produtoPrev.matricula.toString(),
-                codPlano: produtoPrev.codigoPlano
-            }
-    
             const responseDetalheProdutos = await fetch(apiUrlDetalhe, {
                 method: 'POST', // Opcional, GET é o padrão
                 headers: {
@@ -56,7 +21,7 @@ router.post('/dados-previdencia', async (req, res) => {
                     'Authorization': 'Bearer '+ token,
                     'Ocp-Apim-Subscription-Key': process.env.subsciption
                 },
-                body: JSON.stringify(dadosParaConsultaDetalhe)
+                body: JSON.stringify(req.body)
            });
            if (!responseDetalheProdutos.ok) {
                 console.log('deu erro na detalhe');
@@ -64,15 +29,9 @@ router.post('/dados-previdencia', async (req, res) => {
             }
     
            const detalhesProduto = await responseDetalheProdutos.json();
-    
-           const dadosCompletos = {
-            produto: produtoPrev,
-            detalhe: detalhesProduto
-           }
            
-            res.json(dadosCompletos);
+            res.json(detalhesProduto.data);
         
-        }
         
     } catch (error) {
         console.error("Erro ao gerar o hash:", error);
